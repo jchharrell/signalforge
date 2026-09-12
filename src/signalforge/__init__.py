7461f247e245e52431ddec175b115da23e180a37
jchharrell <52176964+jchharrell@users.noreply.github.com>
2026-09-11 22:26:05 -0400

    Export SignalForge monitoring primitives

--- src/signalforge/__init__.py
+++ src/signalforge/__init__.py
@@ -1,4 +1,22 @@
 from .metrics import population_stability_index, rolling_accuracy, latency_percentile
 from .monitor import HealthSnapshot, evaluate_health
+from .fingerprint import DriftFingerprint, FeatureDrift, drift_fingerprint, distribution_distance
+from .canary import CanaryDecision, AlertBudget, evaluate_canary
+from .report import HealthReport, build_health_report
 
-__all__ = ["population_stability_index", "rolling_accuracy", "latency_percentile", "HealthSnapshot", "evaluate_health"]
+__all__ = [
+    "population_stability_index",
+    "rolling_accuracy",
+    "latency_percentile",
+    "HealthSnapshot",
+    "evaluate_health",
+    "DriftFingerprint",
+    "FeatureDrift",
+    "drift_fingerprint",
+    "distribution_distance",
+    "CanaryDecision",
+    "AlertBudget",
+    "evaluate_canary",
+    "HealthReport",
+    "build_health_report",
+]
